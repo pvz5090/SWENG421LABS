a6eabfc025c74049d61a8cec3c62707a55b0e654
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-02-17 17:45:47 -0500

    Minimum skeleton of Edge and Vertex classes

---
+++ M6_lab/M6_lab/Edge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6_lab
+{
+    internal class Vertex
+    {
+        private int vertex_ID;
+        private int x_coordinate;
+        private int y_coordinate;
+
+        public void drawing()
+        { }
+
+        public int getX()
+        { }
+
+        public void setX(int x)
+        { }
+
+        public int getY()
+        { }
+
+        public void setY(int x)
+        { }
+
+        public int getEdgeID()
+        { }
+
+    }
+}
---
+++ M6_lab/M6_lab/Vertex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6_lab
+{
+    internal class Vertex
+    {
+        private int edge_ID;
+        private Vertex from_vertex;
+        private Vertex to_vertex;
+
+        public void drawing()
+        { }
+
+        public void setFromVertex(Vertex fromVertex)
+        { }
+
+        public Vertex getFromVertex()
+        { }
+
+
+        public void setToVertex(Vertex fromVertex)
+        { }
+
+        public Vertex getToVertex()
+        { }
+
+        public int getVertexID()
+        { }
+
+    }
+}
