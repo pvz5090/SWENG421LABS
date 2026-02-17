4a3c8b6f6b5f5c5646f31b1bda736a4da779c3de
Martin Stanton <mss6475@psu.edu>
2026-02-17 18:04:06 -0500

    progess on Graph class

---
+++ M6_lab/M6_lab/Graph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;//for list
+
+  
+public class Graph :ICloneable
+{
+	int graph_ID;
+	List<M6_lab.Vertex> listOfVertices;
+    List<M6_lab.Edge> listOfEdges;
+
+
+
+    public Graph()
+	{
+	}
+
+	public display()
+
+	public clone()
+
+	public getID()
+}
