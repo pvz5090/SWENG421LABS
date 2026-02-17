28e4520bbd9f605e102e98f1611436106324efd1
Patrick Zhao <pvz5090@psu.edu>
2026-02-17 17:22:29 -0500

    m5 Lab

--- m3_lab/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ m3_lab/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f179a33370dd52ca3dd4ee73666e8631e67849f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
