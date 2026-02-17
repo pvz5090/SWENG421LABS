a7152b385a8b1327843f7f55760408f0bc729176
Martin Stanton <mss6475@psu.edu>
2026-02-17 18:31:23 -0500

    Graph progress

--- M6_lab/M6_lab/Graph.cs
+++ M6_lab/M6_lab/Graph.cs
@@ -3,23 +3,42 @@ using System.Numerics;
 using System.Collections.Generic;//for list
 
   
-public class Graph :ICloneable
+internal class Graph :ICloneable
 {
 	int graph_ID;
 	List<M6_lab.Vertex> listOfVertices;
     List<M6_lab.Edge> listOfEdges;
 
 
-        
-    public Graph()
-        {
-            this.graph_ID = id;
-        }
+
+    public Graph(int id)
+	{
+			this.graph_ID = id;
+			List<M6_lab.Vertex> listOfVertices = new List<M6_lab.Vertex>();
+			List<M6_lab.Edge> listOfEdges = new List<M6_lab.Edge>();
     }
 
-	public display()
+	public void addVertex(M6_lab.Vertex v)
+	{
+		listOfVertices.Add(v);
+    }
+
+	public void addEdge(M6_lab.Edge e)
+	{ 
+		listOfEdges.Add(e);
+    }
+
+	public void removeVertex(M6_lab.Vertex v)
+	{ }
+
+
+
+
+
+    public display()
 
 	public clone()
 
 	public getID()
+
 }
