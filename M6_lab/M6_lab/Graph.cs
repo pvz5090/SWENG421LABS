f8a7739f755a415d77c0903f71e12ecc643365a9
Patrick Zhao <pvz5090@psu.edu>
2026-02-17 17:56:40 -0500

    graphManager

---
+++ M6_lab/M6_lab/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6_lab
+{
+    internal class Graph 
+    {
+        private int graph_ID; 
+        
+        public Graph(int id)
+        {
+            this.graph_ID = id;
+        }
+    }
+}
---
+++ M6_lab/M6_lab/GraphManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6_lab
+{
+    internal class GraphManager
+    {
+        private static GraphManager graphManager; 
+        public static List <Graph> listOfGraphs = new List <Graph> (); 
+        private static int create() 
+        {
+            lock (listOfGraphs) {
+                listOfGraphs.Add(new Graph(listOfGraphs.Count + 1));
+                return listOfGraphs.Count - 1;
+            }
+            
+        }
+        private static void modify(int graph_id)
+        {
+
+        }
+
+        private static int copy(int graph_id)
+        {
+            listOfGraphs.Add(listOfGraphs[graph_id - 1].clone());
+            return listOfGraphs.Count - 1;
+        }
+
+    }
+}
