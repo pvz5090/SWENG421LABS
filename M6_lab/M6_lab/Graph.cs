d32ab350a43e39c5b93d159e9604bb4c1d62246b
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-02-24 17:56:18 -0500

    Implement Graph drawing and cloning

--- M6_lab/M6_lab/Graph.cs
+++ M6_lab/M6_lab/Graph.cs
@@ -18,6 +18,18 @@ internal class Graph :ICloneable
 			List<M6_lab.Vertex> listOfVertices = new List<M6_lab.Vertex>();
 			List<M6_lab.Edge> listOfEdges = new List<M6_lab.Edge>();
     }
+    public Graph(Graph otherGraph)
+    {
+
+        Graph newGraph = new Graph(otherGraph.getID() + 1);
+        this.listOfVertices = otherGraph.listOfVertices
+            .Select(v => v.Clone())
+            .ToList();
+
+        this.listOfEdges = otherGraph.listOfEdges
+            .Select(e => e.Clone())
+            .ToList();
+    }
 
 	public void addVertex(M6_lab.Vertex v)
 	{
@@ -32,13 +44,54 @@ internal class Graph :ICloneable
 	public void removeVertex(M6_lab.Vertex v)
 	{ }
 
+    public void display(Graphics g)
+	{
+		// NOTE come back to this and discuss
+        // super.display(g);
 
+		double cosine(Point p1, Point p2) {
+            double d0 = p1.X * p2.X + p1.Y * p2.Y;
+            double d1 = Math.Sqrt(p1.X * p1.X + p1.Y * p1.Y);
+            return d0 / d1;
+        }
 
+		Point compute(Point p1, double angle) {
+            double d1 = Math.Sqrt(p1.X * p1.X + p1.Y * p1.Y);
+            double x = -20 * p1.X / d1;
+            double y = -20 * p1.Y / d1;
+            double nx = x * Math.Cos(angle) - y * Math.Sin(angle);
+            double ny = x * Math.Sin(angle) + y * Math.Cos(angle);
+            return new Point((int) nx, (int) ny);
+        }
 
+        int s = 25;
+        Color color = Color.Black;
+		Pen pen = new Pen(color);
+        Brush brush = new SolidBrush(color);
+        Rectangle r1 = new Rectangle(200, 130, 2 * s, 2 * s);
+        Rectangle r2 = new Rectangle(100, 330, 2 * s, 2 * s);
+        g.DrawEllipse(pen, r1);
+        g.DrawEllipse(pen, r2);
+        
+        int v = r1.Y > r2.Y? -1: 1;
+        double d = cosine(new Point(r2.X - r1.X, r2.Y - r1.Y), new Point(v, 0));
+        double x = r1.X + s + v * s * d;
+        double y = r1.Y + s + v * s * Math.Sqrt(1 - d * d);
+        double x2 = r2.X + s - v * s * d;
+        double y2 = r2.Y + s - v * s * Math.Sqrt(1 - d * d);
+        g.DrawLine(pen, new Point((int) x, (int) y), new Point((int) x2, (int) y2));
+        g.FillEllipse(brush, new Rectangle((int) (x - 5), (int) (y - 5), 10, 10));
 
-    public display()
+        Point p = compute(new Point(r2.X - r1.X, r2.Y - r1.Y), Math.PI / 6);
+        g.DrawLine(pen, new Point((int) x2, (int) y2), new Point((int) x2 + p.X, (int) y2 + p.Y));
+        p = compute(new Point(r2.X - r1.X, r2.Y - r1.Y), -Math.PI / 6);
+        g.DrawLine(pen, new Point((int) x2, (int) y2), new Point((int) x2 + p.X, (int) y2 + p.Y));
+    }
 
-	public clone()
+	public object Clone()
+    {
+        return new Graph(this);
+    }
 
 	public int getID()
 	{
