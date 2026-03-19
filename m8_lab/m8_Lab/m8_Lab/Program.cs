b12dc386c7cfb47c96eaa57b536d2624c75a572e
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-19 15:40:16 -0400

    main program

--- m8_lab/m8_Lab/m8_Lab/Program.cs
+++ m8_lab/m8_Lab/m8_Lab/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace m8_Lab
+{
+    class Program
+    {
+        static void PrintTVInfo(TV_IF tv)
+        {
+            if (tv == null)
+            {
+                Console.WriteLine("TV is null (not within budget)");
+            }
+            else
+            {
+                Console.WriteLine(tv.GetInfo());
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Buyer b = new Buyer();
+            Customer c = new Customer();
+
+            TV_IF tv3 = b.tv.Replenish("Sony", 500);
+            PrintTVInfo(tv3);
+
+            TV_IF tv4 = b.tv.Replenish("LG", 250);
+            PrintTVInfo(tv4);
+
+            TV_IF tv1 = c.tv.Replenish("Sony", 500);
+            PrintTVInfo(tv1);
+
+            TV_IF tv2 = c.tv.Replenish("LG", 250);
+            PrintTVInfo(tv2);
+        }
+    }
+
+}
