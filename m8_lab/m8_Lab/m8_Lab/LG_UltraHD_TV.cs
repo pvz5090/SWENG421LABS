6f8452017acc614f1d1320bb0192db168f2dd45b
Martin Stanton <mss6475@psu.edu>
2026-03-19 15:20:47 -0400

    added barebones of LG_TV, LG_Smart_TV, LG_UltraHD_TV, Sony_TV, and Sony_UltraHD_TV classes

---
+++ m8_lab/m8_Lab/m8_Lab/LG_UltraHD_TV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m8_Lab
+{
+    internal class LG_UltraHD_TV
+    {
+    }
+}
