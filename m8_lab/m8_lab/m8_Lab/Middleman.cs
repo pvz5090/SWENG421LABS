856ea92e2eb2838fb7243693b5414bf106c41264
Patrick Zhao <pvz5090@psu.edu>
2026-03-19 15:26:44 -0400

    top five of chart

---
+++ m8_lab/m8_lab/m8_Lab/Middleman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m8_Lab
+{
+    internal class Middleman : TV, TV_IF
+    {
+        public List<TV> tvTable;
+        private TV tv; 
+        public string GetBrand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TV Replenish(string type, int budget)
+        {
+            throw new NotImplementedException();
+        }
+
+        string TV_IF.GetType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
