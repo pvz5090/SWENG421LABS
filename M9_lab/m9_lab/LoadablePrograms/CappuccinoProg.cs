bd9ee372e9ae240d2b523272468cb13adbcb6126
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-24 17:27:37 -0400

    More consistency with assignment description in LoadablePrograms

--- M9_lab/m9_lab/LoadablePrograms/CappuccinoProg.cs
+++ M9_lab/m9_lab/LoadablePrograms/CappuccinoProg.cs
@@ -8,5 +8,12 @@ namespace m9_lab
 {
     internal class CappuccinoProg:LoadableClasses
     {
+        public override void run()
+        {
+            enviroment.setLEDnum(2);
+            enviroment.setGrindingTime(6);
+            enviroment.setTemperature(140);
+            enviroment.setLEDnum(-1);
+        }
     }
 }
--- M9_lab/m9_lab/LoadablePrograms/EspressoProg.cs
+++ M9_lab/m9_lab/LoadablePrograms/EspressoProg.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace m9_lab
 {
-    internal class RegularProg:LoadableClasses
+    internal class EspressoProg:LoadableClasses
     {
         public override void run()
         {
-            enviroment.setLEDnum(0);
-            enviroment.setGrindingTime(10);
-            enviroment.setTemperature(180);
+            enviroment.setLEDnum(3);
+            enviroment.setGrindingTime(5);
+            enviroment.setTemperature(200);
             enviroment.setLEDnum(-1);
         }
     }
