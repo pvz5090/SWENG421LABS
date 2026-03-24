6152413ac73e89dc1fdf1b4f534cd2023f400717
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-24 16:57:03 -0400

    Remove GUI features

--- M9_lab/m9_lab/ProgramMain.cs
+++ M9_lab/m9_lab/ProgramMain.cs
@@ -2,16 +2,9 @@ namespace m9_lab
 {
     internal static class ProgramMain
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Console.WriteLine("Hello world");
         }
     }
 }
