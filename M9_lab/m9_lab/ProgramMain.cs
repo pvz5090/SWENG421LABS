220c5208e931118207be746be4201709d8bbe700
Martin Stanton <mss6475@psu.edu>
2026-03-24 17:37:43 -0400

    starteed implementing main method

--- M9_lab/m9_lab/ProgramMain.cs
+++ M9_lab/m9_lab/ProgramMain.cs
@@ -5,6 +5,42 @@ namespace m9_lab
         static void Main()
         {
             Console.WriteLine("Hello world");
+
+            CoffeeMakingMachineEnviroment cme = new CoffeeMakingMachineEnviroment();
+            cme.setProgram(1);
+            cme.setCoffee(new Coffee(new Milk()));
+
+            Cream cream = new Cream();
+            Chocholate chocholate = new Chocholate();
+            Vanilla vanilla = new Vanilla();
+            
+            Coffee order1Base=new Coffee();
+            order1Base.addCondiment(cream);
+            order1Base.addCondiment(vanilla);
+
+
+
+            Coffee order2Base = new Coffee();
+            order2Base.addCondiment(cream);
+            order2Base.addCondiment(vanilla);
+            order2Base.addCondiment(chocholate);
+
+
+            /*
+             * 
+             * Prepare and print prices for:
+
+Mocha with:
+
+Cream ×1
+Vanilla ×1
+
+Espresso with:
+
+Cream ×1
+Vanilla ×1
+Chocolate ×2
+             */
         }
     }
 }
