a9f8b1ca07d9efc3afd5b528d12a8756a3463089
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-26 09:50:17 -0400

    Create initial WorkerIF and Employee

---
+++ m10_lab/m10_lab/EmployeeIFs/WorkerIF.cs
@@ -0,0 +1,8 @@
+﻿namespace m10_lab
+{
+    public interface WorkerIF
+    {
+        public void SeeDanger();
+        public void Evacuate();
+    }
+}
---
+++ m10_lab/m10_lab/Employees/Employee.cs
@@ -0,0 +1,48 @@
+﻿namespace m10_lab
+{
+    public abstract class Employee : WorkerIF
+    {
+        private List<WorkerIF> _subordinates = new List<WorkerIF>();
+        private WorkerIF? _superior = null;
+        private string _name;
+        public Employee(List<WorkerIF> subordinates, WorkerIF? superior, string name)
+        {
+            _subordinates = subordinates;
+            _superior = superior;
+            _name = name;
+        }
+        public void Evacuate()
+        {
+            foreach (WorkerIF s in _subordinates)
+            {
+                s.Evacuate();
+            }
+            Evacuate();
+        }
+
+        public void SeeDanger()
+        {
+            throw new NotImplementedException();
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void AddSuperior(Employee employee)
+        {
+            _superior = employee;
+            employee.AddSubordinate(this);
+        }
+        protected void AddSubordinate(Employee employee)
+        {
+            _subordinates.Add(employee);
+        }
+    }
+
+}
+
