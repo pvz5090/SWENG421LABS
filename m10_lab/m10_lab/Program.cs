02804ff8fc9f4b3479a2f4d0a8cea617641eee0c
Martin Stanton <mss6475@psu.edu>
2026-03-31 18:48:18 -0400

    Created Employ classes instances in Program

--- m10_lab/m10_lab/Program.cs
+++ m10_lab/m10_lab/Program.cs
@@ -20,37 +20,37 @@ Denise leads: Amy, Wil, Adam
  */
 
 CEO Steve = new CEO("Steve");
+    //emplyees under Steve
 
-Manager Bob = new Manager( Steve, "Bob");
-Manager Rachel = new Manager(Steve, "Rachel");
+    Manager Bob = new Manager( Steve, "Bob");
+        //employees under Bob
 
-Supervisor Jack = new Supervisor(JackSub, null, "Jack");
-Supervisor Jeff = new Supervisor(JeffSub, null, "Jeff");
-ProjectLeader Chuck = new ProjectLeader(ChuckSub, null, "Chuck");
-ProjectLeader Denise = new ProjectLeader(DeniseSub, null, "Denise");
+        Supervisor Jack = new Supervisor(Bob, "Jack");
+            //employee under Jack
+            Worker John = new Worker(Jack, "John");
+            Worker Mary = new Worker(Jack, "Mary");
+            Worker Jane = new Worker(Jack, "Jane");
 
+        Supervisor Jeff = new Supervisor(Bob, "Jeff");
+            //employee under Jeff
+            Worker Rob = new Worker( Jeff, "Rob");
+            Worker Ed = new Worker( Jeff, "Ed");
+            Worker Rick = new Worker( Jeff, "Rick");
 
+    Manager Rachel = new Manager(Steve, "Rachel");
+        //employees under Rachel
 
-Worker John = new Worker( null, "John");
-Worker Mary = new Worker( null, "Mary");
-Worker Jane = new Worker(null, "Jane");
+        ProjectLeader Chuck = new ProjectLeader( Rachel, "Chuck");
+            //employees under Chuck
+            Worker Joe = new Worker(Chuck, "Joe");
+            Worker Sam = new Worker(Chuck, "Sam");
+            Worker Greg = new Worker(Chuck, "Greg");
 
-
-Worker Rob = new Worker(null, null, "Rob");
-Worker Ed = new Worker(null, null, "Ed");
-Worker Rick = new Worker(null, null, "Rick");
-
-List<Worker> BobSub = new List<Worker>() { Jack, Jeff };
-
-
-
-Worker Joe = new Worker(null, null, "Joe");
-Worker Sam = new Worker(null, null, "Sam");
-Worker Greg = new Worker(null, null, "Greg");
-
-Worker Amy = new Worker(null, null, "Amy");
-Worker Wil = new Worker(null, null, "Wil");
-Worker Adam = new Worker(null, null, "Adam");
+        ProjectLeader Denise = new ProjectLeader(Rachel, "Denise");
+            //employees under Denise
+            Worker Amy = new Worker( Denise, "Amy");
+            Worker Wil = new Worker( Denise, "Wil");
+            Worker Adam = new Worker( Denise, "Adam");
 
 
 
