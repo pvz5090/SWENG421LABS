6d771e5d5412532042a19b37ccc5c1fe8d3485b4
Patrick Zhao <pvz5090@psu.edu>
2026-03-31 17:36:55 -0400

    .

---
+++ m10_lab/m10_lab/Employees/CEO.cs
@@ -0,0 +1,25 @@
+﻿using m10_lab.EmployeeIFs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m10_lab.Employees
+{
+    internal class CEO: Employee, Boss
+    {
+        private Decision decision; 
+        public CEO(List<WorkerIF> subordinates, WorkerIF? superior, string name) : base(List < WorkerIF > subordinates, WorkerIF ? superior, string name)
+        {    
+        }
+        public void seeDanger()
+        {
+
+        }
+        private Decision grant(List<Decision> decs)
+        {
+            return decs[0]; 
+        }
+    }
+}
---
+++ m10_lab/m10_lab/Employees/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m10_lab.Employees
+{
+    internal class Manager: Worker, UpperManagement 
+    {
+        private Worker informedBy;
+        public Manager(List<WorkerIF> subordinates, WorkerIF? superior, string name) : base(subordinates, null, name)
+        {
+
+        }
+        public void SeeDanger()
+        {
+
+        }
+
+        private void ContactBoss()
+        {
+
+        }
+        public Decision suggestedDecision()
+        {
+            return new Decision();
+        }
+        public void addInformer(WorkerIF informer) { informedBy = informer; }
+    }
+}
