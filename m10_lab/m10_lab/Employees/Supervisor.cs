b3447b4e42ba1ee9e5234eb33aee58d07bfdaac9
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-31 17:32:17 -0400

    LowerManagement and Supervisor

---
+++ m10_lab/m10_lab/Employees/Supervisor.cs
@@ -0,0 +1,30 @@
+using System.Transactions;
+
+namespace m10_lab
+{
+    public class Supervisor : Worker, LowerManagement
+    {
+        private UpperManagement _superior;
+
+        public Supervisor(List<Worker> subordinates, UpperManagement? superior, string name)
+        {
+            _superior = superior;
+        }
+        public void Evacuate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SeeDanger()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ProvideInfo()
+        {
+            return "INFO";
+        }
+    }
+
+}
+
