174b8aa6467e55160b3ca8cf425d49349487955b
Martin Stanton <mss6475@psu.edu>
2026-03-31 18:13:57 -0400

    fixed project leader See danger method

--- m10_lab/m10_lab/Employees/ProjectLeader.cs
+++ m10_lab/m10_lab/Employees/ProjectLeader.cs
@@ -25,7 +25,7 @@ namespace m10_lab.Employees
             return ("Information from " + this._name);
         }
 
-        public void seeDanger()
+        public void SeeDanger()
         {   
             if (superior == null)
             {
@@ -37,7 +37,7 @@ namespace m10_lab.Employees
                 {
                     s.FixIt();//solve the problem
                 }
-                superior.seeDanger();//report to superiot the problem
+                superior.SeeDanger();//report to superiot the problem
             }
         }
     }
