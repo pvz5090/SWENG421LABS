02c433fe73737d7724af003c054cb6eca95ac588
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-03-31 17:52:13 -0400

    Worker class

---
+++ m10_lab/m10_lab/Employees/Worker.cs
@@ -0,0 +1,22 @@
+﻿using m10_lab.EmployeeIFs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace m10_lab.Employees
+{
+    internal class Worker: Employee
+    {
+        private _superior LowerManagement;
+        public Worker(List<WorkerIF> subordinates, UpperManagement? superior, string name): base
+        {
+            _subordinates = subordinates;
+            _superior = superior;
+            _name = name;
+        }
+
+    }
+}
