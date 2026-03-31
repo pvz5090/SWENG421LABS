c0a54eddba34eef7d96398ebc18a7342f9b87cf4
Martin Stanton <mss6475@psu.edu>
2026-03-31 18:30:35 -0400

    progress on  main

--- m10_lab/m10_lab/Program.cs
+++ m10_lab/m10_lab/Program.cs
@@ -22,20 +22,27 @@ Denise leads: Amy, Wil, Adam
 Worker John = new Worker(null, null, "John");
 Worker Mary = new Worker(null, null, "Mary");
 Worker Jane = new Worker(null, null, "Jane");
-WorkerIF[] JackSub = { John, Mary, Jane };
+List<Worker> JackSub =new List<Worker>() { John, Mary, Jane };
+Supervisor Jack = new Supervisor(JackSub, null, "Jack");
 
 
 Worker Rob = new Worker(null, null, "Rob");
 Worker Ed = new Worker(null, null, "Ed");
 Worker Rick = new Worker(null, null, "Rick");
+List<Worker> JeffSub = new List<Worker>() { Rob, Ed, Rick };
+Supervisor Jeff = new Supervisor(JeffSub, null, "Jeff");
 
 Worker Joe = new Worker(null, null, "Joe");
 Worker Sam = new Worker(null, null, "Sam");
 Worker Greg = new Worker(null, null, "Greg");
+List<Worker> ChuckSub = new List<Worker>() { Joe, Sam, Greg };
+ProjectLeader Chuck = new ProjectLeader(ChuckSub, null, "Chuck");
 
 Worker Amy = new Worker(null, null, "Amy");
 Worker Wil = new Worker(null, null, "Wil");
 Worker Adam = new Worker(null, null, "Adam");
+List<Worker> DeniseSub = new List<Worker>() { Amy, Wil, Adam };
+ProjectLeader Denise = new ProjectLeader(DeniseSub, null, "Denise");
 
 
 
