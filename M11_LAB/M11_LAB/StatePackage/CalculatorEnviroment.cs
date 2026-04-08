c6daa7d24e3370f8b772ef0d02ac889001b10526
Martin Stanton <stantonmartin19@gmail.com>
2026-04-08 18:26:46 -0400

    added getter and setters for CalculatorEnviroment

--- M11_LAB/M11_LAB/StatePackage/CalculatorEnviroment.cs
+++ M11_LAB/M11_LAB/StatePackage/CalculatorEnviroment.cs
@@ -8,5 +8,42 @@ namespace M11_LAB.StatePackage
 {
     internal class CalculatorEnviroment
     {
-    }
-}
+        private double firstNumber;
+        private double previousOperator;
+        private double SecondNumber;
+
+
+
+        public double getFirstNumber()
+        {
+            return this.firstNumber;
+
+        }
+
+        public void setFirstNumber(double num)
+        {
+            this.firstNumber = num;
+        }
+
+        public double getPreviousOperator()
+        {
+            return previousOperator;
+        }
+
+        public void setPreviousOperator(double num)
+        {
+            this.previousOperator = num;
+        }
+
+        public double getSecondNumber()
+        {
+            return this.SecondNumber;
+        }
+
+        public void setSecondNumber(double num)
+        {
+            this.SecondNumber = num;
+        }
+
+    }//end class
+}//end namespace
