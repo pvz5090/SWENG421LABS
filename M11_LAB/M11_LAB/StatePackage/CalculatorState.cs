3791c8053fc38dc6050998bf3845e38195895d74
Martin Stanton <stantonmartin19@gmail.com>
2026-04-08 18:18:05 -0400

    added signitures to CalculatorState

--- M11_LAB/M11_LAB/StatePackage/CalculatorState.cs
+++ M11_LAB/M11_LAB/StatePackage/CalculatorState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,30 @@ namespace M11_LAB.StatePackage
 {
     internal class CalculatorState
     {
-    }
+        protected ResultState result;
+        protected OperatorState oper;
+        protected CalculatorEnviroment env;
+        public int minusEvent = 1;
+
+        private create()
+        { 
+        }
+
+        protected enter()
+        { 
+        }
+
+        protected static CalculatorState nextState(int eventID)
+        {
+        
+        }
+
+        public CalculatorState start()
+        { 
+        }
+
+        public CalculatorState ProcessEvent(int eventID)
+        {
+
+        }
 }
