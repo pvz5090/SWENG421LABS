9d12935e27260c58376928f5dcba279aaee9244a
Martin Stanton <stantonmartin19@gmail.com>
2026-04-08 19:48:24 -0400

    some method in operations package are done

--- M11_LAB/M11_LAB/OperationPackage/Square.cs
+++ M11_LAB/M11_LAB/OperationPackage/Square.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using M11_LAB.StatePackage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace M11_LAB.OperationPackage
 {
     internal class Square: UnaryOperatorIF
     {
+        public double ExecuteOperation(CalculatorEnviroment state)
+        {
+            return state.GetFirstNumber() * state.GetFirstNumber();
+        }
     }
 }
