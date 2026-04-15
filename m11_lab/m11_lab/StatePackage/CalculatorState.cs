db7bfb97ce3b43707b368d3d762293f3795e0c51
Patrick Zhao <pvz5090@psu.edu>
2026-04-15 19:28:27 -0400

    s

--- m11_lab/m11_lab/StatePackage/CalculatorState.cs
+++ m11_lab/m11_lab/StatePackage/CalculatorState.cs
@@ -9,7 +9,7 @@ using M11_LAB.OperationPackage;
 
 namespace M11_LAB.StatePackage
 {
-    internal class CalculatorState
+    internal abstract class CalculatorState
     {
         protected ResultState resultState;
        
@@ -19,14 +19,8 @@ namespace M11_LAB.StatePackage
         protected TwoNumberState twoNumberState;
 
         protected CalculatorEnviroment env;
-<<<<<<< HEAD
         public CalculatorState GetNextState(string input, CalculatorEnviroment)
-=======
-        public CalculatorState GetNextState(int input, CalculatorEnviroment)
->>>>>>> 043226ae22d94ac5dd6d673d89ed9e01fb50b669
         {
-
-
         }
 
         public CalculatorState GetNextState(BinaryOperatorIF input, CalculatorEnviroment)
--- m11_lab/m11_lab/StatePackage/TwoNumberState.cs
+++ m11_lab/m11_lab/StatePackage/TwoNumberState.cs
@@ -12,10 +12,11 @@ namespace M11_LAB.StatePackage
         public CalculatorState GetNextState(string input)
         {
             if(input == "backspace"){
-                env.PopFromFirstNumber(input);
+                env.PopFromSecondNumber();
             }
             else {
-                env.PushToFirstNumber(input);
+                env.PushToSecondNumber(input);
+                env.setDisplay(env.GetSecondNumber());
             }
 
             return twoNumberState;
@@ -23,25 +24,31 @@ namespace M11_LAB.StatePackage
 
         public CalculatorState GetNextState(BinaryOperatorIF input)
         {
+            input.ExecuteOperation(env);
+
             return operatorState;
         }
 
         public CalculatorState GetNextState(UnaryOperatorIF input)
         {
+            input.ExecuteOperation(env);
             return unaryState;
         }
 
         public CalculatorState GetNextState(CE input)
         {
+            input.ExecuteOperation(env);
             return twoNumberState;
         }
 
         public CalculatorState GetNextState(C input)
         {
+            input.ExecuteOperation(env);
             return operatorState;
         }
         public CalculatorState GetNextState(Equal input)
         {
+            input.ExecuteOperation(env);
             return resultState;
         }
     }
